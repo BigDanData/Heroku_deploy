d8df5ef8c2b05b23cf91849d664b9fdf7c3410d7
Daniel Marin <daniel.marin.chicharro@pragsis.com>
2021-05-11 12:43:04 +0200

    last changes

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-#importing libraries
+#imports
 import flask
 import pandas as pd
 import joblib
 from flask import Flask, render_template, request
-#creating instance of the class
-app=Flask(__name__)
-#to tell flask what url shoud trigger the function index()
+#crear la instancia del servidor
+app = Flask(__name__)
+
+#le indicamos a flask la url que debe lanzar con la función index
 @app.route('/')
 @app.route('/index')
 def index():
   return flask.render_template('index.html')
+
 def ValuePredictor(to_predict_list):
   a = [to_predict_list]
   b = ["variable a", "variable b", "variable c"]
   to_predict = pd.DataFrame(a, columns=b)
   for col in b:
     to_predict[col] = to_predict[col].astype('float')
-  loaded_model = joblib.load("modelo_2.pkl")
+  loaded_model = joblib.load("modelo.pkl")
   result = loaded_model.predict_proba(to_predict)[:,1]
   return result[0]
+
 @app.route('/result', methods = ['POST'])
 def result():
   if request.method == 'POST':
@@ -27,11 +30,10 @@ def result():
 
     try:
       prediction = ValuePredictor(to_predict_list)
-      #prediction = to_predict_list
 
     except ValueError:
       prediction="Error en el formato de los datos"
     return render_template("result.html", prediction=prediction)
 
 if __name__=="__main__":
-  app.run(debug=False, port=5000)
+  app.run(debug=True, port=5000)
